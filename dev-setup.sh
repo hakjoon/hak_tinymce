2cbf01d846503143c0c9c8e29d02499cbe64a20c
hakjoon <public@hakjoon.com>
2009-09-20 10:57:38 -0400

    make symlink actually work

--- dev-setup.sh
+++ dev-setup.sh
@@ -10,4 +10,6 @@ mkdir $DEV_DIR
 echo "copying distro folder to dev root"
 cp -r $DISTRO_DIR $DEV_DIR
 echo "symlink php script in dev folder"
+cd $DEV_DIR
 ln -s hak_tinymce.php $DEV_DIR/hak_tinymce.php
+cd ..
