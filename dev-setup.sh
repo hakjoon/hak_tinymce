2b36865feacadc2cd2c15840c3cd920e682f8159
hakjoon <public@hakjoon.com>
2009-09-20 10:51:05 -0400

    update dev setup script to create things in a dev folder

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-TINYMCE_DIR="tiny_mce"
-DISTRO_DIR="tinymce-distro/jscripts/$TINYMCE_DIR"
+DEV_DIR="dev"
+DISTRO_DIR="tinymce-distro/jscripts/tiny_mce"
 
-echo "removing old tinymce folder"
-rm -r $TINYMCE_DIR
-echo "copying distro folder to plugin root"
-cp -r $DISTRO_DIR $TINYMCE_DIR
+echo "removing old dev folder"
+rm -r $DEV_DIR
+echo "creating new dev folder"
+mkdir $DEV_DIR
+echo "copying distro folder to dev root"
+cp -r $DISTRO_DIR $DEV_DIR
+echo "symlink php script in dev folder"
+ln -s hak_tinymce.php $DEV_DIR/hak_tinymce.php
