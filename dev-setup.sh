619fbeef4b34075787a1b899aed3ee7133373e7a
hakjoon <public@hakjoon.com>
2009-10-07 23:04:03 -0400

    remove compressed advimage plugin replace with uncompressed

--- dev-setup.sh
+++ dev-setup.sh
@@ -5,6 +5,8 @@ DEV_DIR="$CURRENT_DIR/dev"
 TINYMCE_DIR="tiny_mce"
 DISTRO_DIR="$CURRENT_DIR/tinymce-distro/jscripts/$TINYMCE_DIR"
 COMPRESSOR_DIR="$CURRENT_DIR/tinymce_compressor_php"
+DEV_PLUGIN_DIR="$DEV_DIR/$TINYMCE_DIR/plugins"
+TXPIMAGE_DIR="$CURRENT_DIR/txpimage"
 
 echo "removing old dev folder "
 rm -r $DEV_DIR
@@ -14,6 +16,8 @@ echo "copying distro folder to dev root"
 cp -r $DISTRO_DIR $DEV_DIR
 echo "copying compressor to dev dir"
 cp $COMPRESSOR_DIR/tiny_mce_gzip.* $DEV_DIR/$TINYMCE_DIR
+echo "copying txpimage into tinymce directory"
+cp -r $TXPIMAGE_DIR $DEV_PLUGIN_DIR
 echo "symlink php script in dev folder"
 #cd $DEV_DIR
 ln -s $CURRENT_DIR/hak_tinymce.php $DEV_DIR/hak_tinymce.php
