7d81892b78fdd2b24219d1bb60df6cdf44ef046c
hakjoon <public@hakjoon.com>
2009-10-07 23:00:00 -0400

    renamed advimage instances to txpimage

--- txpimage/editor_plugin_src.js
+++ txpimage/editor_plugin_src.js
@@ -6,18 +6,18 @@
  */
 
 (function() {
-	tinymce.create('tinymce.plugins.AdvancedImagePlugin', {
+	tinymce.create('tinymce.plugins.TxpImagePlugin', {
 		init : function(ed, url) {
 			// Register commands
-			ed.addCommand('mceAdvImage', function() {
+			ed.addCommand('mceTxpImage', function() {
 				// Internal image object like a flash placeholder
 				if (ed.dom.getAttrib(ed.selection.getNode(), 'class').indexOf('mceItem') != -1)
 					return;
 
 				ed.windowManager.open({
 					file : url + '/image.htm',
-					width : 480 + parseInt(ed.getLang('advimage.delta_width', 0)),
-					height : 385 + parseInt(ed.getLang('advimage.delta_height', 0)),
+					width : 480 + parseInt(ed.getLang('txpimage.delta_width', 0)),
+					height : 385 + parseInt(ed.getLang('txpimage.delta_height', 0)),
 					inline : 1
 				}, {
 					plugin_url : url
@@ -26,22 +26,22 @@
 
 			// Register buttons
 			ed.addButton('image', {
-				title : 'advimage.image_desc',
-				cmd : 'mceAdvImage'
+				title : 'txpimage.image_desc',
+				cmd : 'mceTxpImage'
 			});
 		},
 
 		getInfo : function() {
 			return {
-				longname : 'Advanced image',
-				author : 'Moxiecode Systems AB',
-				authorurl : 'http://tinymce.moxiecode.com',
-				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/advimage',
-				version : tinymce.majorVersion + "." + tinymce.minorVersion
+				longname : 'Textpattern image',
+				author : 'Patrick Woods',
+				authorurl : 'http://www.hakjoon.com',
+				infourl : 'http://hakjoon.com/code/haktinymce',
+				version : 0.2
 			};
 		}
 	});
 
 	// Register plugin
-	tinymce.PluginManager.add('advimage', tinymce.plugins.AdvancedImagePlugin);
+	tinymce.PluginManager.add('txpimage', tinymce.plugins.TxpImagePlugin);
 })();
