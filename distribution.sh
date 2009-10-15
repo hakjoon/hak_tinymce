6c8cd297f0df52599a600527968310bf42ba4f8e
hakjoon <public@hakjoon.com>
2009-10-14 21:46:27 -0400

    fix distribution script

--- distribution.sh
+++ distribution.sh
@@ -2,7 +2,7 @@
 
 NAME="hak_tinymce"
 CURRENT_DIR=$(pwd)
-DIST_DIR="$CURRENT_DIR/$NAME-dist"
+DIST_DIR="$NAME-dist"
 TPL_DIR="$CURRENT_DIR/template"
 TINYMCE_DIR="tiny_mce"
 DISTRO_DIR="$CURRENT_DIR/tinymce-distro/jscripts/$TINYMCE_DIR"
@@ -10,11 +10,11 @@ COMPRESSOR_DIR="$CURRENT_DIR/tinymce_compressor_php"
 DEV_PLUGIN_DIR="$DIST_DIR/$TINYMCE_DIR/plugins"
 TXPIMAGE_DIR="$CURRENT_DIR/txpimage"
 
-echo "removing old dev folder "
+echo "removing old dist folder "
 rm -r $DIST_DIR
-echo "creating new dev folder"
+echo "creating new dist folder"
 mkdir $DIST_DIR
-echo "copying distro folder to dev root"
+echo "copying distro folder to dist root"
 cp -r $DISTRO_DIR $DIST_DIR
 echo "copying compressor to dev dir"
 cp $COMPRESSOR_DIR/tiny_mce_gzip.* $DIST_DIR/$TINYMCE_DIR
@@ -24,9 +24,9 @@ echo "create distribution"
 cp $NAME.php $TPL_DIR
 cd $TPL_DIR
 php $NAME.php > $NAME.txt
-mv $NAME.txt $DIST_DIR
+mv $NAME.txt $CURRENT_DIR/$DIST_DIR
 rm $NAME.php
 cd $CURRENT_DIR
 cp install.txt $DIST_DIR
-zip $NAME.zip $DIST_DIR
+zip -r $NAME.zip $DIST_DIR
 rm -r $DIST_DIR
